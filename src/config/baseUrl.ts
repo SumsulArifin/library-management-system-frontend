96cdfa541941b094e41cd4295fec30b9f1b478f6
ArifinSumsul <s.arifinoriginal@gmail.com>
2025-10-23 12:42:22 +0600

    .

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://l2-b5-library-management-api.vercel.app/api";
+export const BASE_URL = "https://library-management-system-sigma-nine.vercel.app/api";
 // export const BASE_URL = "http://localhost:3000/api";
